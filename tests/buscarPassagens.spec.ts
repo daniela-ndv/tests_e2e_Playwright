1d8d1383970cf5a445d23c0392ebca598c9f59d6
Daniela Dalla Vecchia <114515986+daniela-ndv@users.noreply.github.com>
2024-10-31 17:45:55 -0300

    teste buscar_passagens

---
+++ tests/buscarPassagens.spec.ts
@@ -0,0 +1,19 @@
+import {test} from '@playwright/test';
+import PaginaPrincipal from './page-objects/paginaPrincipal';
+
+test.describe('Buscar passagens', ( )=> {
+    test('Deve buscar passagens somente de ida', async ({ page }) => {
+        const paginaPrincipal = new PaginaPrincipal(page);
+
+        await paginaPrincipal.visitar();
+        await paginaPrincipal.definirSomenteIda();
+        await paginaPrincipal.abrirModalPassageiros();
+        await paginaPrincipal.definirPassageirosAdultos(3);
+        await paginaPrincipal.definirPassageirosCriancas(1);
+        await paginaPrincipal.definirPassageirosBebes(1);
+        await paginaPrincipal.fecharModalPassageiros();
+
+        await paginaPrincipal.definirOrigemEDestino('minas gerais', 'rio de janeiro');
+
+    });
+});
