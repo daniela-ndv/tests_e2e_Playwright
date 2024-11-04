7329afdfe5a1ee9357b0a41299716f4653880aeb
Daniela Dalla Vecchia <114515986+daniela-ndv@users.noreply.github.com>
2024-11-04 14:13:57 -0300

    Login e logout

--- tests2/fazerLogin.spec.ts
+++ tests2/fazerLogin.spec.ts
@@ -1,12 +1,16 @@
-import test, { expect } from "@playwright/test";
-import PaginaLogin from "./page-objects/paginaLogin";
+import { test } from './page-objects/paginaLogin.ts';
 
 test.describe('fazer_login', () => {
-    test('Deve realizar login', async ({ page }) => {
-        const paginaLogin = new PaginaLogin(page);
-        await paginaLogin.visitar();
-        await paginaLogin.fazerLogin('playwright@testes.com', 'login');
+    test('Deve realizar login', async ({ paginaLogin }) => {
+        await paginaLogin.fazerLogin('playwright@testes.com', 'admin');
         await paginaLogin.verificarLogin();        
     })
+
+    test('Deve realizar login e logout', async ({ paginaLogin}) => {
+        await paginaLogin.fazerLogin('playwright@testes.com', 'admin');
+        await paginaLogin.verificarLogin();  
+        await paginaLogin.fazerLogout();
+        await paginaLogin.verificarLogout();
+     })   
 
 })
--- tests2/page-objects/paginaLogin.ts
+++ tests2/page-objects/paginaLogin.ts
@@ -1,4 +1,5 @@
-import {test as base, expect, Locator, Page} from '@playwright/test';
+import {expect, Locator, Page} from '@playwright/test';
+import {test as base} from '@playwright/test';
 
 export const test = base.extend<{ paginaLogin: PaginaLogin}>({
     paginaLogin: async ({ page }, use) => {
@@ -13,18 +14,23 @@ export default class PaginaLogin {
     private readonly inputEmail: Locator;
     private readonly inputSenha: Locator;
     private readonly botaoLogin: Locator;
+    private readonly botaoConta: Locator;
+    private readonly botaoLogout: Locator;
+    private readonly msgWelcome: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.inputEmail = page.locator('input#username');
         this.inputSenha = page.locator('input#password');
         this.botaoLogin = page.getByRole('button', { name: 'Sign in' });
+        this.botaoConta =  page.getByTitle('playwright@testes.com (playwright@testes.com)')
+        this.botaoLogout = page.getByRole('link', { name: ' Logout' });
+        this.msgWelcome = page.getByRole('heading', { name: 'Welcome!' });
     }
 
     async visitar(){
-        await this.page.goto('/');  
-        const msgWelcome = await this.page.getByRole('heading', { name: 'Welcome!' });    
-        await expect(msgWelcome).toBeVisible();  
+        await this.page.goto('/');    
+        await expect(this.msgWelcome).toBeVisible();  
     }
 
     async fazerLogin(email: string, senha:string){
@@ -36,6 +42,16 @@ export default class PaginaLogin {
     async verificarLogin(){
         const homePage = await this.page.getByRole('link', { name: 'Playwright Testes' })
         await expect(homePage).toBeVisible()
+    }
+
+    async fazerLogout(){
+        await this.page.waitForTimeout(2000);
+        await this.botaoConta.click();
+        await this.botaoLogout.click();
+    }
+
+    async verificarLogout(){
+        await expect(this.msgWelcome).toBeVisible();  
     }
     
 }
